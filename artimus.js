6f94ffb5d0ef2d972cd461af30861b9719e78d8c
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-11-14 13:11:21 -0500

    Hello Artimus

---
+++ artimus.js
@@ -0,0 +1,253 @@
+window.artimus = {
+    tools: {},
+
+    workspace: class {
+        //Scrolling
+        #scrollX = 0;
+        set scrollX(value) {
+            this.#scrollX = value;
+            this.updatePosition();
+        }
+        get scrollX() { return this.#scrollX; }
+
+        #scrollY = 0;
+        set scrollY(value) {
+            this.#scrollY = value;
+            this.updatePosition();
+        }
+        get scrollY() { return this.#scrollY; }
+
+        #zoom = 2;
+        set zoom(value) {
+            this.#zoom = Math.max(Math.min(value, 25), 0.25);
+            this.updatePosition();
+        }
+        get zoom() { return this.#zoom; }
+
+        #tool = ""
+        set tool(value) {
+            if (artimus.tools[value]) this.toolFunction = artimus.tools[value];
+            else this.toolFunction = {};
+
+            Object.assign(this.toolProperties, this.toolFunction.properties);
+
+            this.#tool = value;
+            this.refreshToolOptions();
+        }
+        get tool() { return this.#tool; }
+
+        toolProperties = {};
+
+        updatePosition() {
+            //Setup some CSS
+            this.quickVar(this.container, {
+                "scrollX": `${this.scrollX}px`,
+                "scrollY": `${this.scrollY}px`,
+                "zoom": this.zoom
+            });
+        }
+
+        //Our own quickCSS function, for use outside of coffee engine.
+        quickCSS(element, css) {
+            if (Array.isArray(element)) {
+                for (let elID in element) {
+                    editor.quickCSS(element[elID], css);
+                }
+            }
+
+            if (!(element instanceof HTMLElement)) return element;
+
+            for (let key in css) {
+                element.style[key] = css[key];
+            }
+
+            return element;
+        }
+
+        //Quickly sets css variables
+        quickVar(element, variables) {
+            if (Array.isArray(element)) {
+                for (let elID in element) {
+                    editor.quickVar(element[elID], variables);
+                }
+            }
+
+            if (!(element instanceof HTMLElement)) return element;
+
+            for (let key in variables) {
+                element.style.setProperty(`--${key}`, variables[key]);
+            }
+
+            return element;
+        }
+
+        RGBtoHex(RGB) {
+            let hexR = Math.floor(RGB.r).toString(16);
+            let hexG = Math.floor(RGB.g).toString(16);
+            let hexB = Math.floor(RGB.b).toString(16);
+
+            if (hexR.length == 1) hexR = "0" + hexR;
+            if (hexG.length == 1) hexG = "0" + hexG;
+            if (hexB.length == 1) hexB = "0" + hexB;
+
+            //Transparency
+            if (RGB.a) {
+                let hexA = Math.floor(RGB.a).toString(16);
+                if (hexA.length == 1) hexA = "0" + hexA;
+
+                return `#${hexR}${hexG}${hexB}${hexA.toLowerCase() == "ff" ? "" : hexA}`;
+            }
+
+            return `#${hexR}${hexG}${hexB}`;
+        }
+
+        constructor(overrideCSS) {
+            overrideCSS = overrideCSS || {};
+
+            //Look for existing CUGI if one doesn't exist, add it.
+            if (!window.CUGI) {
+                window.CUGI = {};
+
+                const CUGIScript = document.createElement("script");
+                CUGIScript.src = "https://coffee-engine.github.io/CUGI/CUGI.js";
+                document.body.appendChild(CUGIScript);
+            }
+
+            this.createLayout(overrideCSS);
+            this.addControls();
+
+            this.GL = this.canvas.getContext("2d");
+            //Sometimes we need this. Sometimes we don't?
+            //I dunno, just no IE11
+            //this.GL.translate(-0.5, -0.5);
+            this.GL.imageSmoothingEnabled = false;
+        }
+
+        createLayout(overrideCSS) {
+            //Create needed elements
+            this.container = document.createElement("div");
+            this.toolbar = document.createElement("div");
+            this.canvasArea = document.createElement("div");
+
+            this.canvas = document.createElement("canvas");
+            
+            //Now we can style our children
+            this.container.className = "artimus-container";
+            this.toolbar.className = "artimus-toolbar";
+            this.canvasArea.className = "artimus-canvasArea";
+            this.canvas.className = "artimus-canvas";
+            
+            this.container.appendChild(this.toolbar);
+            this.container.appendChild(this.canvasArea);
+            this.canvasArea.appendChild(this.canvas);
+
+            this.updatePosition();
+        }
+
+        addControls() {
+            //Drawing
+            this.canvas.addEventListener("contextmenu", (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+            });
+
+            this.canvas.addEventListener("mousedown", (event) => {
+                switch (event.button) {
+                    case 0:
+                        if (this.toolFunction.mouseDown && !this.toolDown) this.toolFunction.mouseDown(this.GL, ...this.getCanvasPosition(event.clientX, event.clientY), this.toolProperties);
+                        this.toolDown = true;
+                        break;
+
+                    case 2:
+                        const [red, green, blue, alpha] = this.GL.getImageData(...this.getCanvasPosition(event.clientX, event.clientY, true), 1, 1).data;
+                        const converted = this.RGBtoHex({ r:red, g:green, b:blue, a:alpha });
+
+                        this.toolProperties.strokeColor = converted;
+                        this.toolProperties.fillColor = converted;
+
+                        //Refresh options
+                        this.refreshToolOptions();
+                        break;
+                
+                    default:
+                        break;
+                }
+            });
+            this.canvas.addEventListener("mouseup", (event) => {
+                if (event.button != 0) return;
+                
+                if (this.toolFunction.mouseUp && this.toolDown) this.toolFunction.mouseUp(this.GL, ...this.getCanvasPosition(event.clientX, event.clientY), this.toolProperties);
+                this.toolDown = false; 
+            });
+            this.canvas.addEventListener("mouseout", (event) => { 
+                if (this.toolFunction.mouseUp && this.toolDown) this.toolFunction.mouseUp(this.GL, ...this.getCanvasPosition(event.clientX, event.clientY), this.toolProperties);
+                this.toolDown = false; 
+            });
+            this.canvas.addEventListener("mousemove", (event) => {
+                if (this.toolDown && this.toolFunction.mouseMove) this.toolFunction.mouseMove(this.GL, ...this.getCanvasPosition(event.clientX, event.clientY), event.movementX / this.zoom, event.movementY / this.zoom, this.toolProperties);
+            });
+
+            //Add movement
+            this.container.addEventListener("mousedown", (event) => {
+                if (event.button == 1) {
+                    const moveEvent = (event) => {
+                        this.scrollX += event.movementX / this.zoom;
+                        this.scrollY += event.movementY / this.zoom;
+                    }
+
+                    const upEvent = (event) => {
+                        if (event.button == 1) {
+                            document.removeEventListener("mousemove", moveEvent);
+                            document.removeEventListener("mouseup", upEvent);
+                        }
+                    }
+
+                    //Bind events
+                    document.addEventListener("mousemove", moveEvent);
+                    document.addEventListener("mouseup", upEvent)
+                }
+            });
+
+            this.canvasArea.addEventListener("wheel", (event) => {
+                if (event.ctrlKey) {
+                    event.preventDefault();
+                    this.zoom += event.deltaY / -100;
+                }
+                else if (event.shiftKey) {
+                    this.scrollX -= (event.deltaY) / this.zoom;
+                    this.scrollY -= (event.deltaX) / this.zoom;
+                    this.zoom += event.deltaZ / -100;
+                }
+                else {
+                    this.scrollX -= (event.deltaX) / this.zoom;
+                    this.scrollY -= (event.deltaY) / this.zoom;
+                    this.zoom += event.deltaZ / -100;
+                }
+            }, { passive: false });
+        }
+
+        getCanvasPosition(x, y) {
+            this.#scrollX = -x;
+            this.#scrollX = -y;
+        }
+
+        getCanvasPosition(x, y) {
+            const {top, left} = this.canvas.getBoundingClientRect();
+            return [Math.floor((x - Math.floor(left)) / this.zoom), Math.floor((y - Math.floor(top)) / this.zoom)];
+        }
+
+        refreshToolOptions() {
+            this.toolbar.innerHTML = "";
+            this.toolbar.appendChild(CUGI.createList(this.toolFunction.CUGI(this)));
+        }
+    },
+
+    inject: (element, overrideCSS) => {
+        if (!(element instanceof HTMLElement)) return;
+
+        const workspace = new artimus.workspace(overrideCSS);
+        element.appendChild(workspace.container);
+
+        return workspace;
+    }
+}
---
+++ tools/paintBrush.js
@@ -0,0 +1,87 @@
+artimus.tools.paintBrush = {
+    icon: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="70.5" height="70.5" viewBox="0,0,70.5,70.5"> <g transform="translate(-204.75002,-144.75)">    <g data-paper-data="{&quot;isPaintingLayer&quot;:true}" fill-rule="nonzero" stroke="none" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" style="mix-blend-mode: normal">        <path d="M204.75003,215.25v-70.5h70.5v70.5z" fill="none" stroke-width="0"/>        <path d="M220.9868,205.17696c1.77179,-0.89842 3.45323,-2.83003 3.92284,-4.18449c0.48941,-2.20805 2.09187,-5.70927 4.03585,-6.94886c1.41138,-1.79045 6.7982,-2.72387 8.25105,-0.51354c3.63129,2.41038 4.42564,4.90457 4.65906,6.97496c0.87449,2.30301 -2.19833,6.25534 -4.02505,7.55363c-2.70649,1.77061 -6.09868,1.76254 -9.25182,2.13584c-3.36677,0.39859 -5.03047,-0.4888 -7.98273,-1.41774c-0.53432,-0.4212 -3.55958,-2.15572 -3.34232,-2.965c0.23096,-0.8603 2.73102,-0.52502 3.38089,-0.60196l0.28441,-0.03367c0,0 0.02808,-0.00332 0.06782,0.00082z" fill="currentColor" stroke-width="0.5"/>        <path d="M254.7307,185.57527c-5.41655,12.21861 -8.83657,10.44178 -13.17454,8.51874c-4.33797,-1.92303 -7.95119,-3.26405 -2.53464,-15.48266c5.41655,-12.21861 17.81172,-30.68787 22.14969,-28.76483c4.33797,1.92304 -1.02396,23.51014 -6.4405,35.72876z" fill="currentColor" stroke-width="0"/></g></g></svg><!--rotationCenter:35.249975000000006:35.25000499999999-->',
+    mouseDown: (gl, x, y, toolProperties) => {
+        //if (toolProperties.pixelBrush) { x--; y--; };
+        //Set stroke properties
+        gl.lineCap = "round";
+        gl.lineJoin = "round";
+        gl.lineWidth = toolProperties.strokeSize;
+        gl.strokeStyle = toolProperties.strokeColor;
+        gl.fillStyle = toolProperties.strokeColor;
+
+        //Then start drawing
+        toolProperties.linePos = [x,y];
+
+        //For the smooth brush
+        if (!toolProperties.pixelBrush) {
+            gl.moveTo(x,y);
+            gl.beginPath();
+            gl.lineTo(x,y);
+            gl.stroke();
+        }
+        else {
+            //Calculations
+            const halfSize = Math.floor(toolProperties.strokeSize / 2);
+            const rx = x - halfSize;
+            const ry = y - halfSize;
+
+            gl.fillRect(rx,ry,toolProperties.strokeSize,toolProperties.strokeSize);
+        }
+    },
+    mouseMove: (gl, x, y, vx, vy, toolProperties) => {
+        if (toolProperties.pixelBrush) {
+            //For non-AA line drawing;
+            const {linePos, strokeSize} = toolProperties;
+            const halfSize = Math.floor(strokeSize / 2);
+            const distance = 1 / Math.sqrt(Math.pow(linePos[0] - x, 2.0) + Math.pow(linePos[1] - y, 2.0));
+
+            //Draw the line
+            for (let i = 0; i < 1; i+=distance) {
+                const rx = Math.floor((linePos[0] + (x - linePos[0]) * i) - halfSize);
+                const ry = Math.floor((linePos[1] + (y - linePos[1]) * i) - halfSize);
+
+                gl.fillRect(rx,ry,strokeSize,strokeSize);
+            }
+
+            toolProperties.linePos = [x,y];
+        }
+        //Smooth brush
+        else {
+            const {linePos} = toolProperties;
+            const distance = Math.sqrt(Math.pow(linePos[0] - x, 2.0) + Math.pow(linePos[1] - y, 2.0));
+
+            //Assure we don't overdraw
+            if (distance > 1) {
+                gl.lineTo(x,y);
+                gl.stroke();
+
+                gl.closePath(); //! We do it in this order or else firefox throws a fit.
+                gl.beginPath();
+                gl.moveTo(x,y);
+
+                toolProperties.linePos = [x,y];
+            }
+        }
+    },
+    mouseUp: (gl, x, y, toolProperties) => {
+        if (toolProperties.pixelBrush) { x++; y++; };
+        if (!toolProperties.pixelBrush) {
+            gl.lineTo(x,y);
+            gl.stroke();
+            gl.moveTo(x,y);
+            gl.closePath();
+        }
+    },
+
+    CUGI:(artEditor) => { return [
+        { target: artEditor.toolProperties, key: "strokeColor", type: "color" },
+        { target: artEditor.toolProperties, key: "strokeSize", type: "int" },
+        { target: artEditor.toolProperties, key: "pixelBrush", type: "boolean" },
+    ]},
+
+    properties: {
+        strokeColor: "#000000",
+        strokeSize: 2,
+        pixelBrush: false,
+    }
+}
